5962cd2226fe141efe3bdde7b7f4a7c9a5a8adb7
montheramer <monther.amer48@gmail.com>
2021-07-06 23:38:19 +0300

    add cors middleware to allow cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require('cors');
 
 // * require database configration
 require("./config/index");
@@ -9,6 +10,8 @@ require("./config/index");
 const indexRouter = require("./routes");
 
 const app = express();
+
+app.use(cors());
 
 app.use(logger("dev"));
 app.use(express.json());
