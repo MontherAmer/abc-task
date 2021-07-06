ffa32da1c6f3634aec35bb39709733af41c41b6a
montheramer <monther.amer48@gmail.com>
2021-07-07 02:11:27 +0300

    fixing heroku

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const cors = require('cors');
+const cors = require("cors");
 
 // * require database configration
 require("./config/index");
@@ -20,6 +20,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public/build")));
 
 app.use("/apis", indexRouter);
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public/build/index.html')));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/build/", "index.html"))
+);
 
 module.exports = app;
