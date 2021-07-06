2dc93f1f45698091202a19ae0354e851258aa264
montheramer <monther.amer48@gmail.com>
2021-07-07 01:33:28 +0300

    add response static file and update heroku pre build in package json

--- app.js
+++ app.js
@@ -17,8 +17,9 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public/build")));
 
 app.use("/apis", indexRouter);
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/build/index.html')));
 
 module.exports = app;
