28d0cbb79a3696d3e91e40e82fcfd034807b588d
montheramer <monther.amer48@gmail.com>
2021-07-06 20:02:45 +0300

    set database configration

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// * require database configration
+require('./config/index')
+
+var indexRouter = require('./routes');
 
 var app = express();
 
@@ -14,7 +16,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/apis', indexRouter);
 
 module.exports = app;
