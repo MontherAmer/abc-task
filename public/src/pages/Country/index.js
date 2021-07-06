23029e37f308744e273d3231599345fc758cd77b
montheramer <monther.amer48@gmail.com>
2021-07-06 21:17:56 +0300

    add public folder

---
+++ public/src/pages/Country/index.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+const Country = () => {
+  return (
+    <div>
+      Country CountryCountry
+      <p>CountryCountryCountryCountryCountryCountryCountryCountryCountry</p>
+      <p>CountryCountryCountryCountryCountryCountryCountryCountryCountry</p>
+      <p>CountryCountryCountryCountryCountryCountryCountryCountryCountry</p>
+      <p>CountryCountryCountryCountryCountryCountryCountryCountryCountry</p>
+      <p>CountryCountryCountryCountryCountryCountryCountryCountryCountry</p>
+      <p>CountryCountryCountryCountryCountryCountryCountryCountryCountry</p>
+      <p>CountryCountryCountryCountryCountryCountryCountryCountryCountry</p>
+      <p>CountryCountryCountryCountryCountryCountryCountryCountryCountry</p>
+    </div>
+  )
+}
+
+export default Country
